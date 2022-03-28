a00d38485f9d6b16d4d94c729f509c1f4dfe5e0c
Jannik Obermann <jannik.obermann@gmail.com>
2022-03-28 18:48:04 +0200

    Add extra Args to useFilePicker hook

--- web/rt-easy/src/hooks/useFilePicker.tsx
+++ web/rt-easy/src/hooks/useFilePicker.tsx
@@ -1,36 +1,42 @@
 import { useCallback } from "react";
 
 export type Accept = "*" | string[];
-export interface Options {
+export interface Options<Args extends unknown[]> {
   accept?: "*" | string[];
-  onChange: (name: string, content: string) => void;
+  onChange: (name: string, content: string, ...args: Args) => void;
 }
-export type FilePicker = () => void;
+export type FilePicker<Args extends unknown[]> = (...args: Args) => void;
 
-export function useFilePicker({ accept, onChange }: Options): FilePicker {
-  const openFilePicker = useCallback(() => {
-    const inputElement = document.createElement("input");
+export function useFilePicker<Args extends unknown[]>({
+  accept,
+  onChange,
+}: Options<Args>): FilePicker<Args> {
+  const openFilePicker = useCallback(
+    (...args: Args) => {
+      const inputElement = document.createElement("input");
 
-    inputElement.type = "file";
-    inputElement.multiple = false;
-    inputElement.accept =
-      accept === undefined || accept === "*" ? "*" : accept.join(",");
+      inputElement.type = "file";
+      inputElement.multiple = false;
+      inputElement.accept =
+        accept === undefined || accept === "*" ? "*" : accept.join(",");
 
-    inputElement.addEventListener("change", () => {
-      const files = inputElement.files ? Array.from(inputElement.files) : [];
-      if (files.length === 1) {
-        const file = files[0];
-        const reader = new FileReader();
-        reader.onload = () => {
-          const content = reader.result as string;
-          onChange(file.name, content);
-        };
-        reader.readAsText(file);
-      }
-    });
+      inputElement.addEventListener("change", () => {
+        const files = inputElement.files ? Array.from(inputElement.files) : [];
+        if (files.length === 1) {
+          const file = files[0];
+          const reader = new FileReader();
+          reader.onload = () => {
+            const content = reader.result as string;
+            onChange(file.name, content, ...args);
+          };
+          reader.readAsText(file);
+        }
+      });
 
-    inputElement.click();
-  }, [accept, onChange]);
+      inputElement.click();
+    },
+    [accept, onChange]
+  );
 
   return openFilePicker;
 }
